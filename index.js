536a0e06c11bb03dd63dcdf9c6de2e28606c9c5b
Shel Orock <sdo243@nyu.edu>
2018-04-18 03:09:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,12 +46,12 @@ function removeFromCart(item) {
   for(var i = 0; i < cart.length; i++) {
     var checkItem = cart[i].itemName;
     if(item === checkItem) {
-      var updatedCart = cart.splice(i, 1);
+      cart.splice(i, 1);
+      return cart;
     } else {
       return 'That item is not in your cart.'
     }
   }
-  return updatedCart;
 }
 
 function placeOrder(cardNumber) {
